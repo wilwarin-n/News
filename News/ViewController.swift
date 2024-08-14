3e956cd6b0cc549a1005eee9b9d8247da2a67d33
Rumeysa <michiru.aware.lrn@gmail.com>
2024-08-14 16:59:36 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Aydın Kutlu on 14.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
